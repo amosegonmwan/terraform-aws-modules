994aa5a9cb96b1185292228f49470e0b01406f75
Amos Egonmwan <amos.egonmwan@hotmail.com>
2024-07-26 23:56:22 -0600

    chore: S3 backend for terraform Contributing-to: https://wemadevops.atlassian.net/jira/software/projects/WD102/boards/4?selectedIssue=WD102-47

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "topic" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:wema-topic"]
+    #resources = ["arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:wema-topic"]
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "topic_name" {
+  type = string
+  default = "my-topic"
+}
+
+variable "team" {
+  type = string
+  #default = "dev"
+}
+
+variable "my_bucket" {
+  type = string
+  #default = "wemadevop"
+}
+
+variable "env" {
+  type = string
+  #default = "prod"
+}
